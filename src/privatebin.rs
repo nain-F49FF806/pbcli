b5e8eaa39b0fc190e148bed2dcec800ddf0d12f4
nain <126972030+nain-F49FF806@users.noreply.github.com>
2024-04-07 06:56:33 +0200

    fix rebase, PostPasteResponse is object

--- src/privatebin.rs
+++ src/privatebin.rs
@@ -127,7 +127,7 @@ pub type DecryptedCommentsMap = HashMap<String, DecryptedComment>;
 pub type CommentsAdjacencyMap = HashMap<String, Vec<String>>;
 
 
-#[derive(Deserialize, Debug, Serialize, Clone)]
+#[derive(Deserialize, Debug, Serialize, Clone, uniffi::Object)]
 pub struct PostPasteResponse {
     pub deletetoken: String,
     pub id: String,
