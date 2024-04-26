357284a7f2966b1e2166e0f1829d3294c900df88
nain <126972030+nain-F49FF806@users.noreply.github.com>
2024-04-26 13:25:38 +0200

    uniffi api interface constructor

--- src/uniffi_exports.rs
+++ src/uniffi_exports.rs
@@ -1,18 +1,42 @@
+use crate::{api, opts, uniffi_custom::Url};
+
 /// Simpler interfaces exported to uniffi
 /// "inner" correspond to the native library structs
 
+#[derive(uniffi::Object)]
+pub struct API {
+    inner: api::API,
+}
+
+#[uniffi::export]
+impl API {
+    /// Construct new API instance for given url
+    #[uniffi::constructor]
+    fn new(url: Url) -> API {
+        Self {
+            inner: api::API::new(url, opts::Opts::default()),
+        }
+    }
+}
+
 #[derive(uniffi::Record)]
 pub struct Opts {
-    format: crate::privatebin::PasteFormat,
+    // todo: set default enum variant once supported by uniffi
+    format: crate::PasteFormat,
+    #[uniffi(default = "1week")]
     expire: String,
+    #[uniffi(default = false)]
     burn: bool,
+    #[uniffi(default = false)]
     discussion: bool,
+    #[uniffi(default = None)]
     password: Option<String>,
 }
 
 impl Opts {
-    fn get_inner(&self) -> crate::opts::Opts {
-        crate::opts::Opts {
+    /// get native library version of Opts
+    fn get_inner(&self) -> opts::Opts {
+        opts::Opts {
             format: self.format,
             expire: self.expire.clone(),
             burn: self.burn,
