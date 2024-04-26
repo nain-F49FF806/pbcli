b56fa3af1fcafaa838cafa4da0f7c579334224e0
nain <126972030+nain-F49FF806@users.noreply.github.com>
2024-04-26 21:41:08 +0200

    add formatted output for read comments and paste

--- src/uniffi_exports.rs
+++ src/uniffi_exports.rs
@@ -3,19 +3,12 @@
 
 use std::collections::HashMap;
 
+use serde_json::json;
+
 use crate::{
     api, opts, uniffi_custom::Url, DecryptedComment, DecryptedPaste, PasteError, PbResult,
     PostPasteResponse,
 };
-
-#[derive(Debug, uniffi::Record)]
-struct ReadPasteOutput {
-    decrypted_paste: DecryptedPaste,
-    // id -> decrypted_comment
-    decrypted_comments: HashMap<String, DecryptedComment>,
-    // id -> [children ids]
-    comment_tree: HashMap<String, Vec<String>>,
-}
 
 #[uniffi::export]
 fn read_paste(paste_url: &Url, password: &Option<String>) -> PbResult<ReadPasteOutput> {
@@ -57,13 +50,11 @@ fn read_paste(paste_url: &Url, password: &Option<String>) -> PbResult<ReadPasteO
             comment_children_map.entry(parentid).or_default().push(id);
         }
     }
-    Ok(
-        ReadPasteOutput{
-            decrypted_paste,
-            decrypted_comments: decrypted_comments_map,
-            comment_tree: comment_children_map
-        }
-    )
+    Ok(ReadPasteOutput {
+        decrypted_paste,
+        decrypted_comments: decrypted_comments_map,
+        comment_tree: comment_children_map,
+    })
 }
 
 #[uniffi::export]
@@ -75,6 +66,57 @@ fn write_paste(
     let password = opts.password.as_deref().unwrap_or("");
     let api = api::API::new(host.clone(), opts.into());
     api.post_paste(content, password, &opts.into())
+}
+
+#[derive(Debug, uniffi::Record)]
+struct ReadPasteOutput {
+    decrypted_paste: DecryptedPaste,
+    // id -> decrypted_comment
+    decrypted_comments: HashMap<String, DecryptedComment>,
+    // id -> [children ids]
+    comment_tree: HashMap<String, Vec<String>>,
+}
+
+impl ReadPasteOutput {
+    fn format_paste(&self) -> String {
+        serde_json::to_string_pretty(&self.decrypted_paste).unwrap()
+    }
+
+    fn format_comments(&self) -> String {
+        use serde_json::Value;
+
+        fn format_comments_below_id(
+            id: &str,
+            decrypted_comments: &HashMap<String, DecryptedComment>,
+            comment_tree: &HashMap<String, Vec<String>>,
+        ) -> Value {
+            let children_array: Vec<Value> = comment_tree
+                .get(id)
+                .unwrap_or(&Vec::new())
+                .iter()
+                .map(|child_id| {
+                    format_comments_below_id(child_id, decrypted_comments, comment_tree)
+                })
+                .collect();
+
+            let comment_content =
+                serde_json::to_value(decrypted_comments.get(id).unwrap_or(&DecryptedComment {
+                    comment: "".to_owned(),
+                    nickname: None,
+                }))
+                .unwrap();
+
+            json!({
+                id: {
+                    "content": comment_content,
+                    "children": children_array
+                }
+            })
+        }
+        let comments_json =
+            format_comments_below_id("", &self.decrypted_comments, &self.comment_tree);
+        serde_json::to_string_pretty(&comments_json).unwrap()
+    }
 }
 
 #[derive(uniffi::Record)]
